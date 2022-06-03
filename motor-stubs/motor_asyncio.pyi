56e147e75703bd3ea1a94ff5dd5ee4c3e82910ae
Daniel Hsiao (蕭翊安) <daniel.hsiao@tcstock.com.tw>
2022-06-03 18:01:13 +0800

    fix: missing database pyi

--- motor-stubs/motor_asyncio.pyi
+++ motor-stubs/motor_asyncio.pyi
@@ -1,9 +1,10 @@
 from typing import TypeVar
 
-from .core import AgnosticCollection
+from .core import AgnosticDatabase, AgnosticCollection
 
 T = TypeVar('T')
 
 def create_asyncio_class(cls: T) -> T: ...
 
+AsyncIOMotorDatabase: AgnosticDatabase
 AsyncIOMotorCollection: AgnosticCollection
