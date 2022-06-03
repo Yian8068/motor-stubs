5c4cf700cb60d4878494d3a2bf99f2059caef807
Daniel Hsiao (蕭翊安) <daniel.hsiao@tcstock.com.tw>
2022-06-03 16:19:17 +0800

    bump: version 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_data = {'': ['*']}
 
 setup_kwargs = {
     'name': 'motor-stubs-test',
-    'version': '0.1.0',
+    'version': '0.2.0',
     'description': '',
     'long_description': None,
     'author': 'Daniel Hsiao',
